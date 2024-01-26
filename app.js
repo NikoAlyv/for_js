a20eb2f35b974f29e88099a3c1b46e0fea56774a
NikoAlyv <enihad801@gmail.com>
2024-01-26 05:32:11 +0400

    Task1

---
+++ app.js
@@ -0,0 +1,54 @@
+//TODO: Task1
+
+//! Part1
+//  const dayOfWeek ="Friday";
+
+//  switch(dayOfWeek){
+//     case "Monday":
+//     console.log("Study JavaScript");
+//     break
+//     case "Tuesday":
+//     console.log("Read book")
+//     break
+//     case "Wednesday":
+//     console.log("Do sport");
+//     break;
+//     case "Thursday":
+//     console.log("Walk interesting place");
+//     break;
+//     case "Friday":
+//     console.log("Go to the mosque");
+//     break;
+//     default:
+//         console.log("Bele bir activite gunu yoxdur");
+//  }
+
+//! Part 2
+// const numbers = [25,89,78,54,26];
+// let sum = 0;
+// for(let i = 0;i<numbers.length;i++)
+// {
+// sum += numbers[i];
+// }
+// console.log(sum);
+
+//! Part 3
+// const fruits =["Apple","Banana","Cherry"];
+
+// for(let i=0;i<fruits.length;i++){
+//     console.log(i+":"+fruits[i]);
+// }
+
+//! Part 4
+
+// const student ={
+//     name:"Nihad",
+//     grade:89
+// }
+
+// if(student.grade>=60){
+// console.log(student.name+" Kecir");
+// }
+// else{
+// console.log(student.name+" Qalir");
+// }
